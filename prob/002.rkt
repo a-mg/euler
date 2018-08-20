2d1915e70eb94c87814f1ce809b9ac0da598363f
A. Mattson Gallagher <ndrwg@me.com>
2018-08-20 23:25:17 +0100

    Solve problem 002

---
+++ prob/002.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require "../util/fibonacci.rkt")
+
+;;; 'Climb' the Fibonacci sequence, given a starting index and a
+;;; maximum value, constructing a list of the found values.
+(define (climb-fibonaccis index limit llist)
+  ;; Calculate the next Fibonacci number.
+  (let ([next (fibonacci index)])
+    (if (< next limit)
+        ;; Next is allowable, so add it to the list and continue.
+        (climb-fibonaccis (+ 1 index)
+                          limit
+                          (append llist `(,next)))
+        ;; Otherwise, return the list as it exists.
+        llist)))
+
+;;; Find the sum of the even valued terms in a list of
+;;; Fibonacci numbers below 4,000,000.
+(foldl + 0 (filter even? (climb-fibonaccis 1 4000000 '())))
