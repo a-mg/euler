9c8fc677aa9fc7fde3b6aa65d68f29dd67e1b7b4
A. Mattson Gallagher <ndrwg@me.com>
2018-08-21 14:31:52 +0100

    Add utility to read a grid of numbers into a two-dimensional list

--- util/data.rkt
+++ util/data.rkt
@@ -1,9 +1,21 @@
 #lang racket
 
-(provide read-number)
+(provide read-number
+         read-grid)
 
 ;;; Reads a number from a file, containing that number on
 ;;; a single line.
 (define (read-number path)
   (with-input-from-file path
     (lambda () (string->number (read-line)))))
+
+;;; Reads a grid of numbers from a file: each line is a list
+;;; of numbers, contained within a wrapping list.
+(define (read-grid path)
+  ;; Helper, converts strings in nested list to numbers.
+  (define (make-integers l)
+    (map string->number l))
+  ;; Read in the grid of numbers.
+  (map make-integers
+       (map string-split
+            (port->lines (open-input-file path)))))
