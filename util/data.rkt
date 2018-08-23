d8e9be0a50e5e8dcfb7b834f786380b686313f61
A. Mattson Gallagher <ndrwg@me.com>
2018-08-23 17:42:21 +0100

    Rewrite data utilities using threading

--- util/data.rkt
+++ util/data.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require threading)
+
 (provide read-number
          read-numbers
          read-csv-strings
@@ -8,21 +10,26 @@
 ;;; Reads a number from a file, containing that number on
 ;;; a single line.
 (define (read-number path)
-  (with-input-from-file path
-    (lambda () (string->number (read-line)))))
+  (~> path
+      open-input-file
+      read-line
+      string->number))
 
 ;;; Reads a list of numbers from a file, one number per line.
 (define (read-numbers path)
-  (map string->number
-       (port->lines (open-input-file path))))
+  (~>> path
+       open-input-file
+       port->lines
+       (map string->number)))
 
 ;;; Reads a list of quoted strings that are separated by commas,
 ;;; splitting on the commas and trimming the quotes.
 (define (read-csv-strings path)
-  (string-split (string-replace (port->string (open-input-file path))
-                                "\""
-                                "")
-                ","))
+  (~> path
+      open-input-file
+      port->string
+      (string-replace "\"" "")
+      (string-split ",")))
 
 ;;; Reads a grid of numbers from a file: each line is a list
 ;;; of numbers, contained within a wrapping list.
@@ -31,6 +38,8 @@
   (define (make-integers l)
     (map string->number l))
   ;; Read in the grid of numbers.
-  (map make-integers
-       (map string-split
-            (port->lines (open-input-file path)))))
+  (~>> path
+       open-input-file
+       port->lines
+       (map string-split)
+       (map make-integers)))
