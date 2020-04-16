5dafc91324b700ee007e415e0a5425c93c4dba8c
A. Mattson Gallagher <amg@a-m-g.org>
2020-04-16 14:18:05 -0400

    improve python algorithm

--- src/005.py
+++ src/005.py
@@ -6,39 +6,32 @@
 # What is the smallest positive number that is evenly divisible by all of the
 # numbers from 1 to 20?
 
-def factors(num):
-    # dictionary of prime factors and exponents
-    # 10 => {2: 1, 5: 1}
-    factors = {}
-    prime   = 2
-    while prime <= num:
-        if num % prime == 0:
-            num /= prime
-            if prime in factors:
-                factors[prime] += 1
-            else:
-                factors[prime] = 1
+from functools import reduce
+
+primes_powers = {}
+
+# iterate over the range to check (excluding 1)
+for number in range(2, 20):
+    prime = 2
+    power = 0
+    while prime <= number:
+        if number % prime == 0:
+            # if prime is a factor of number, divide it out and count how many
+            # times it multiplies into the number
+            number /= prime
+            power += 1
+            if (prime in primes_powers and power > primes_powers[prime]) \
+                    or (prime not in primes_powers):
+                # this captures the highest power of each prime factor of
+                # numbers in the range
+                primes_powers[prime] = power
         else:
             prime += 1
-    return factors
+            power = 0
 
-# get all factorizations in 2..20
-factorizations = [factors(n) for n in range(2, 20)]
+# raise primes to respective powers
+raised_primes = [prime ** power for prime, power in primes_powers.items()]
 
-# find max power for each prime
-reduced = {}
-for item in factorizations:
-    for prime, power in item.items():
-        if (prime in reduced and power > reduced[prime]) \
-                or (prime not in reduced):
-            reduced[prime] = power
-
-# calculate powers
-reduced = [prime ** power for prime, power in reduced.items()]
-
-# calculate product
-product = 1
-for r in reduced:
-    product *= r
-
+# calculate product (solution)
+product = reduce(lambda a, b: a * b, raised_primes)
 print(product)
